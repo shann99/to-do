74a2240ac34f5cd5d6dfe9099dd15eb959b0d58d
Shannon Reid <shannon.victoria.reid@gmail.com>
2022-06-23 13:35:28 -0400

    adding file

---
+++ to_do.py
@@ -0,0 +1,8 @@
+#get user input on what they want to save as a to-do
+#save to-dos in a list
+#if its in a list, it already has a number: 
+#1. get position of the lists and return them + the actual to-do
+#2. ask which to-do they want to cross off by the number
+#3. save those crossed-off to-dos in another list, so when they want to see them: they're crossed off ones are all there
+#make it pretty lol
+#ask if user would like to add another to-do
